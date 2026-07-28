d07c51be7876ccd3ff64b9a364942c2e804fea1e
Augusto <augustogasparetto10@gmail.com>
2026-07-28 15:40:02 -0300

    feat: criação dos principais endpoints e persistência das informações básicas

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+import "dotenv/config"
 import express from "express";
+import prisma from "./prisma/prisma";
 
 const app = express()
-
 app.use(express.json())
 
 app.get("/", (req, res) => {
@@ -10,13 +11,50 @@ app.get("/", (req, res) => {
     });
 });
 
-app.post("/checklists", (req, res) => {
-    console.log(req.body);
+app.get("/items", async (req, res) => {
+    const items = await prisma.item.findMany();
+    return res.json(items)
+});
 
-    res.status(201).json({
-        message: "Checklist criado!",
-        body: req.body
+app.get("/checklists", async (req, res) => {
+    const checklists = await prisma.checklist.findMany();
+    return res.json(checklists)
+});
+
+app.post("/items", async (req, res) => {
+    const { description } = req.body;
+    
+    if(!description){
+        return res.status(400).json({ error: "description is required"});
+    }
+
+    const newItem = await prisma.item.create({
+        data: { description }
     });
+
+    return res.status(201).json(newItem)
+});
+
+app.post("/checklists", async (req, res) => {
+    const { name, description, status } = req.body;
+
+    if(!name){
+        return res.status(400).json({ error: "name is required"});
+    }
+
+    if(!status){
+        return res.status(400).json({ error: "status is required"});
+    }
+
+    if(status !== "RASCUNHO" && status !== "PRONTO"){
+        return res.status(400).json({error: "status 'RASCUNHO' or 'PRONTO' is required"})
+    }
+
+    const newChecklist = await prisma.checklist.create({
+        data: { name, description, status }
+    });
+
+    return res.status(201).json(newChecklist)
 });
 
 app.listen(3000, () => {
